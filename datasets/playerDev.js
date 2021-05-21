993072ce7707600119030e22e630c6dc600665cf
XeonLyfe <xeonlyfeapple@gmail.com>
2021-05-21 18:02:01 -0400

    ads
    asd

--- datasets/playerDev.js
+++ datasets/playerDev.js
@@ -1,6 +1,5 @@
 var playerDev = {
     "000XeonLyfe" : "Owner/Main Dev",
-    "001Hollow" : "Developer",
     "002IronException" : "Developer",
     "003VacCat" : "Developer",
     "004_bt" : "Developer"
---
+++ datasets/versionstest.js
@@ -0,0 +1,14 @@
+var Xenonverr = {
+    "Dev Stage" : [
+        "0.0.9",
+        "0.0.6",
+        "0.0.5 (Lost To Time)",
+        "0.0.4 (Lost To Time)",
+        "0.0.3 (Lost To Time)",
+        "0.0.2 (Lost To Time)",
+        "0.0.1 (Lost To Time)"
+    ],
+    "Release" : [
+        "None Yet as the client is not public :/"
+    ]
+}
